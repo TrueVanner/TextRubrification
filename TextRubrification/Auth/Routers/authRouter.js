eb693f7d4518427b0ea1bd232f753a74d3692826
Vannername <vannername@gmail.com>
2022-01-27 08:43:28 +0200

    first changes

--- TextRubrification/Auth/Routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router;
-const contoller = require("../Controllers/authController");
-
-const router = new Router();
-
-router.post("/login", contoller.login);
-router.get("/getAdmins", authMiddleware, contoller.getAdmins);
-
-module.exports = router;
